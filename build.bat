5fd21a411d150c1af70113fb3ec50594bf5cbc63
Matthew Newton <mcn4@leicester.ac.uk>
2012-03-23 23:25:49 +0000

    add batch script to build msi

---
+++ build.bat
@@ -0,0 +1,7 @@
+
+candle edusetup.wxs -ext WixUtilExtension -ext WixIIsExtension -ext WixUIExtension
+
+light -sice:ICE20 edusetup.wixobj -ext WixUtilExtension -ext WixIIsExtension -ext WixUIExtension
+
+pause
+
